de5e7a81ad7a001697d878116721cd1c52f54740
Naufal101106 <naufalxpplg1@gmail.com>
2025-02-17 13:03:08 +0700

    halaman beranda

--- lib/beranda.dart
+++ lib/beranda.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:ukk_2025/main.dart';
 
-void main() {
-  runApp(MyApp());
+void main () {
+  runApp(const MyWidget());
+  
 }
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
 
-class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,7 +26,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _selectedIndex = 0;
+  int _selectIndex = 0;
 
   final List<Widget> _pages = [
     ProductsPage(),
@@ -34,9 +35,11 @@ class _HomePageState extends State<HomePage> {
     SalesDetailPage(),
   ];
 
+  final GlobalKey<_ProductsPageState> _productsPageKey = GlobalKey();
+
   void _onItemTapped(int index) {
     setState(() {
-      _selectedIndex = index;
+      _selectIndex = index;
     });
   }
 
@@ -52,43 +55,40 @@ class _HomePageState extends State<HomePage> {
           children: [
             DrawerHeader(
               decoration: BoxDecoration(
-                color: Colors.blue,
+                color: Colors.green,
               ),
               child: Text(
                 'Menu Navigasi',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 24,
-                ),
+                style: TextStyle(color: Colors.white, fontSize: 24),
               ),
             ),
             ListTile(
-              leading: Icon(Icons.shopping_bag),
-              title: Text('Produk'),
+              leading: const Icon(Icons.shopping_bag),
+              title: const Text('Produk'),
               onTap: () {
                 _onItemTapped(0);
                 Navigator.pop(context);
               },
             ),
             ListTile(
-              leading: Icon(Icons.people),
-              title: Text('Pelanggan'),
+              leading: const Icon(Icons.people),
+              title: const Text('Pelanggan'),
               onTap: () {
                 _onItemTapped(1);
                 Navigator.pop(context);
               },
             ),
             ListTile(
-              leading: Icon(Icons.receipt),
-              title: Text('Penjualan'),
+              leading: const Icon(Icons.people),
+              title: const Text('Penjualan'),
               onTap: () {
                 _onItemTapped(2);
                 Navigator.pop(context);
               },
             ),
             ListTile(
-              leading: Icon(Icons.details),
-              title: Text('Detail Penjualan'),
+              leading: const Icon(Icons.people),
+              title: const Text('Detail Penjualan'),
               onTap: () {
                 _onItemTapped(3);
                 Navigator.pop(context);
@@ -97,44 +97,176 @@ class _HomePageState extends State<HomePage> {
           ],
         ),
       ),
-      body: _pages[_selectedIndex],
+      body: _pages[_selectIndex],
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(Icons.shopping_bag),
+            activeIcon: Icon(Icons.shopping_bag, color: Colors.green,),
             label: 'Produk',
-          ),
-          BottomNavigationBarItem(
+           ),
+            BottomNavigationBarItem(
             icon: Icon(Icons.people),
             label: 'Pelanggan',
-          ),
-          BottomNavigationBarItem(
+           ),
+           BottomNavigationBarItem(
             icon: Icon(Icons.receipt),
             label: 'Penjualan',
-          ),
-          BottomNavigationBarItem(
+           ),
+           BottomNavigationBarItem(
             icon: Icon(Icons.details),
-            label: 'Detail Penjualan',
-          ),
+            label: 'Detail Pelanggan',
+           ),
         ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
+        currentIndex: _selectIndex,
+        selectedItemColor: Colors.green,
         onTap: _onItemTapped,
       ),
+
+      floatingActionButton: _selectIndex == 0
+        ?FloatingActionButton(
+          onPressed: () async {
+            String? newProduct = await _showProductDialog(context);
+            if (newProduct != null && newProduct.isNotEmpty) {
+              setState(() {
+                _productsPageKey.currentState?.addProduct(newProduct);
+              });
+            }
+          },
+          backgroundColor: Colors.green,
+          child: const Icon(Icons.add),
+         )
+      :null,
+    );
+  }
+
+  Future<String?> _showProductDialog(BuildContext context, {String? initialText}) async {
+    TextEditingController controller = TextEditingController(text: initialText);
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(initialText == null ? 'Tambah Produk' : 'Edit Produk'),
+          content: TextField(
+            controller: controller,
+            decoration: const InputDecoration(hintText: 'Nama Produk'),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context), 
+              child: const Text('Batal'),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context, controller.text);
+              }, 
+              child: const Text('Simpan'),
+            )
+          ],
+        );
+      }
     );
   }
 }
 
-class ProductsPage extends StatelessWidget {
+class ProductsPage extends StatefulWidget {
+  const ProductsPage({Key? Key}) : super(key: Key);
+
+  @override
+  _ProductsPageState createState () =>  _ProductsPageState();
+  }
+
+class _ProductsPageState extends State<ProductsPage> {
+  List<String> products = [];
+
+  void addProduct(String product) {
+    setState(() {
+      products.add(product);
+    });
+  }
+
+  void editProduct(int index, String newProduct) {
+    setState(() {
+      products[index] = newProduct;
+    });
+  }
+
+  void deleteProduct(int index) {
+    setState(() {
+      products.removeAt(index);
+    });
+  }
+  
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Halaman Produk',
-        style: TextStyle(fontSize: 24),
-      ),
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
+
+  @override
+  Widget  build(BuildContext context) {
+    return Scaffold(
+      body: products.isEmpty
+      ? const Center(
+        child: Text(
+          'Belum ada produk',
+          style: TextStyle(fontSize: 18),
+        ),
+      )
+      : ListView.builder(
+      itemCount: products.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(products[index]),
+          trailing: IconButton(
+            icon: const Icon(Icons.delete, color: Colors.red,
+            ),
+            onPressed: () => deleteProducts(index),
+          ),
+          onTap: () async {
+            String? newProduct = await _showProductDialog(context, initialText: products[index]);
+            if (newProduct != null && newProduct.isNotEmpty) {
+              editProducts(index, newProduct);
+            }
+          },
+        );
+      }
+    ),
     );
   }
+  
+    
+    
+    
+    
+
+Future<String?> _showProductDialog(BuildContext context, {String? initialText}) async {
+  TextEditingController controller = TextEditingController(text: initialText);
+  return showDialog(
+    context: context,
+     builder: (context) {
+      return AlertDialog(
+        title: Text(initialText == null ? 'Tambah Produk' : 'Edit Produk'),
+        content: TextField(
+          controller: controller,
+          decoration: const InputDecoration(hintText: 'Nama Produk'),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: const Text('Batal')
+          ),
+          TextButton(
+            onPressed: () {
+              Navigator.pop(context, controller.text);
+            },
+            child: const Text('Simpan'),
+           )
+        ],
+      );
+     }
+  );
 }
 
 class CustomersPage extends StatelessWidget {
